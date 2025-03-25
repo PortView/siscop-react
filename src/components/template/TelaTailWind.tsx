443bd7aa558a0ef06c72812d07a83a620c905fb2
Roberto Luiz Veiga <portview.me@gmail.com>
2025-03-25 19:35:10 -0300

    TelaTailWind acertada para conter os componentes 02

--- src/components/template/TelaTailWind.tsx
+++ src/components/template/TelaTailWind.tsx
@@ -7,113 +7,129 @@ import ReguaDePag from "./ReguaDePag";
 export default function TelaTailWind() {
     return (
         <>
-            {/* Container principal com flex para garantir que as colunas fiquem lado a lado */}
-            <div className="flex flex-col bg-zinc-400 p-1 2xlb:flex-row 2xlb:flex-wrap">
-                {/* Primeira coluna com largura fixa */}
-                <div className="flex w-[900px] h-[160px] flex-col gap-1 bg-amber-400 p-2 mr-2 mb-2 text-xs">
-                    <div className="flex flex-row gap-1">
-                        <DropDown
-                            largura='280px'
-                            placeholder="Clientes"
-                            label=""
-                            options={[]}
-                            onChange={() => { }}
-                            tamanhoFonte="xs" //| "md" | "lg" | "xl" | "2xl" | "3xl";
-                        />
+            {/* Container principal com largura total e centralização de conteúdo */}
+            <div className="w-full flex justify-center">
+                {/* Container interno com largura máxima e centralização */}
+                <div className="w-full max-w-[1920px] mx-auto">
+                    {/* Container do conteúdo - flex-col em telas menores, organizado em telas grandes */}
+                    <div className="flex flex-col items-center bg-zinc-400 p-1 gap-1 mb-2">
+                        {/* Área superior - containers lado a lado em telas grandes */}
+                        <div className="w-full flex flex-col 2xlb:flex-row 2xl:justify-center gap-1">
+                            {/* Primeira coluna com largura fixa */}
+                            <div className="flex w-[940px] h-[140px] flex-col p-1 bg-amber-400 text-xs rounded-md shadow-md">
+                                <div className="flex flex-row">
+                                    <DropDown
+                                        largura='280px'
+                                        placeholder="Clientes"
+                                        label=""
+                                        options={[]}
+                                        onChange={() => { }}
+                                        tamanhoFonte="xs" //| "md" | "lg" | "xl" | "2xl" | "3xl";
+                                    />
 
-                        <DropDown
-                            largura='120px'
-                            placeholder="UF"
-                            label=""
-                            options={[]}
-                            onChange={() => { }}
-                            value={null}
-                            disabled={false} // Desabilitar o dropdown quando o checkbox estiver marcado
-                            tamanhoFonte="xs"
-                        />
+                                    <DropDown
+                                        largura='120px'
+                                        placeholder="UF"
+                                        label=""
+                                        options={[]}
+                                        onChange={() => { }}
+                                        value={null}
+                                        disabled={false} // Desabilitar o dropdown quando o checkbox estiver marcado
+                                        tamanhoFonte="xs"
+                                    />
 
-                        <CheckBox className="flex ml-2 mr-2.5"
-                            label="Todas Ufs"
-                            labelPosition="left"
-                            checked={false}
-                            onChange={() => { }}
-                            disabled={false} // Desabilitar o checkbox quando nenhum cliente estiver selecionado
-                            tamanhoFonte="xs"
-                        />
-                        <Botao
-                            texto="Planilhas"
-                            className="ml-2"
-                            cor="bg-blue-500"
-                            redondo={false}
-                            tamanho="xs"
-                            larguraFixa={110}
-                            icone={<IconFileText />}
-                            tamanhoIcone={28} // Definindo um tamanho maior para o ícone
-                        />
-                        <Botao
-                            texto={`Contr:`}
-                            className="ml-2"
-                            cor="bg-blue-500"
-                            redondo={false}
-                            tamanho="xs"
-                            larguraFixa={120}
-                        />
-                    </div>
+                                    <CheckBox className="flex ml-1 mr-1"
+                                        label="Todas Ufs"
+                                        labelPosition="left"
+                                        checked={false}
+                                        onChange={() => { }}
+                                        disabled={false} // Desabilitar o checkbox quando nenhum cliente estiver selecionado
+                                        tamanhoFonte="xs"
+                                    />
+                                    <Botao
+                                        texto="Planilhas"
+                                        className="ml-1"
+                                        cor="bg-blue-500"
+                                        redondo={false}
+                                        tamanho="xs"
+                                        larguraFixa={110}
+                                        icone={<IconFileText />}
+                                        tamanhoIcone={28} // Definindo um tamanho maior para o ícone
+                                    />
+                                    <Botao
+                                        texto={`Contr:`}
+                                        className="ml-1"
+                                        cor="bg-blue-500"
+                                        redondo={false}
+                                        tamanho="xs"
+                                        larguraFixa={120}
+                                    />
+                                </div>
 
-                    <div className="flex flex-row gap-1">
-                        <DropDown
-                            largura='450px'
-                            placeholder="Unidades"
-                            label="Unidades"
-                            containerClassName="flex mt-2 mb-2"
-                            options={[]}
-                            onChange={() => { }}
-                            value={null}
-                        />
+                                <div className="flex flex-row gap-1">
 
-                        <ReguaDePag
-                            paginaAtual={1}
-                            totalPaginas={1}
-                            totalItems={0}
-                            mostrarRegua={true}
-                            onChange={(pagina) => { }}
-                        />
-                    </div>
+                                    <DropDown
+                                        largura='450px'
+                                        placeholder="Unidades"
+                                        label="Unidades"
+                                        containerClassName="flex mt-1 mb-1"
+                                        options={[]}
+                                        onChange={() => { }}
+                                        value={null}
+                                    />
+
+                                    <ReguaDePag
+                                        paginaAtual={1}
+                                        totalPaginas={1}
+                                        totalItems={0}
+                                        mostrarRegua={true}
+                                        onChange={(pagina) => { }}
+                                    />
+                                </div>
 
 
-                    <div className="flex flex-row gap-1 text-xs">
-                        <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Editar</button>
-                        <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Ocorrencias</button>
-                        <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Custos</button>
-                        <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Ord.Compra</button>
-                        <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Edit.Taref.</button>
-                        <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Rescisão</button>
-                        <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Pendenciar</button>
+                                <div className="flex flex-row gap-1 text-xs">
+                                    <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Editar</button>
+                                    <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Ocorrencias</button>
+                                    <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Custos</button>
+                                    <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Ord.Compra</button>
+                                    <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Edit.Taref.</button>
+                                    <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Rescisão</button>
+                                    <button className="inline-block h-6 min-w-[100px] bg-blue-300 align-middle">Pendenciar</button>
+                                </div>
+                            </div>
+
+                            {/* Segunda coluna com largura fixa */}
+                            <div className="flex w-[940px] h-[140px] 2xlb:w-[820px] flex-col gap-1 bg-amber-600 p-2 rounded-md shadow-md">
+                                <div className="flex flex-row gap-1">
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                </div>
+                                <div className="flex flex-row gap-1">
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                    <div className="flex h-6 w-16 bg-blue-300"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Área do meio - containers lado a lado em telas grandes */}
+                        <div className="w-full flex flex-col 2xlb:flex-row 2xl:justify-center gap-1">
+                            <div className="flex w-[940px] h-[360px] bg-blue-200 rounded-md shadow-md">Grade Serviço</div>
+                            <div className="flex w-[940px] h-[360px] 2xlb:w-[820px] bg-green-200 rounded-md shadow-md">Grade Tarefa</div>
+                        </div>
+
+                        {/* Área inferior - container único embaixo */}
+                        <div className="w-full flex justify-center">
+                            <div className="flex h-[360px] w-full max-w-[1764px] bg-orange-300 rounded-md shadow-md">Grade Conformidades</div>
+                        </div>
                     </div>
                 </div>
-
-                {/* Segunda coluna com largura fixa */}
-                <div className="flex w-[900px] h-[160px] flex-col gap-2 bg-amber-600 p-2 mb-2">
-                    <div className="flex flex-row gap-2">
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                        <div className="flex h-6 w-16 bg-blue-300"></div>
-                    </div>
-                </div>
-                {/* Elementos adicionais que ocupam toda a largura em resolução menor */}
-                <div className="flex h-[500px] w-[900px] bg-blue-200 mr-2 mb-2">Grade Serviço</div>
-                <div className="flex h-[500px] w-[900px] bg-green-200 mb-2">Grade Tarefa</div>
-                {/* Elemento que ocupa toda a largura disponível */}
-                <div className="flex h-[300px] w-[1808px] bg-green-200">Grade Conformidades</div>
             </div>
 
             <div className="hidden columns-2">
@@ -121,6 +137,7 @@ export default function TelaTailWind() {
                 <p className="break-inside-avoid-column">Sure, go ahead, laugh...</p>
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
-            </div></>
+            </div>
+        </>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    safelist: [
-        { pattern: /^gap-/ },
-        { pattern: /^w-/ },
-        { pattern: /^h-/ },
-        { pattern: /^text-/ },
-        { pattern: /^bg-/ },
-    ],
-    plugins: [require("tailwind-scrollbar")],
-    theme: {
-        extend: {
-          screens: {
-            '2xlb': '1680px', // você pode usar qualquer nome, mas '2xl' é uma boa escolha
-          },
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    { pattern: /^gap-/ },
+    { pattern: /^w-/ },
+    { pattern: /^h-/ },
+    { pattern: /^text-/ },
+    { pattern: /^bg-/ },
+  ],
+  plugins: [require("tailwind-scrollbar")],
+  theme: {
+    extend: {
+      screens: {
+        '2xlb': '1520px', // você pode usar qualquer nome, mas '2xl' é uma boa escolha
       },
+    },
+  },
 
 };
 module.exports = config;
